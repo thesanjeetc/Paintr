bcfc1b0e3a7be1dd9c72f2b8a0828ca43901fa92
theraspberryguy <21122782+theraspberryguy@users.noreply.github.com>
2020-05-04 18:32:18 +0100

    Sensor Error Handling

--- client/src/Pages/Controller.js
+++ client/src/Pages/Controller.js
@@ -6,7 +6,9 @@ import Pickr from "@simonwep/pickr";
 class Controller extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      error: false,
+    };
 
     let roomID = props.roomID;
 
@@ -33,14 +35,35 @@ class Controller extends React.Component {
       }, 3000);
     }
 
-    /* eslint-disable no-undef */
-    const sensor = new AbsoluteOrientationSensor({
-      frequency: 50,
-    });
+    try {
+      /* eslint-disable no-undef */
+      const sensor = new AbsoluteOrientationSensor({
+        frequency: 50,
+      });
+      sensor.addEventListener("error", (event) => {
+        // Handle runtime errors.
+        if (event.error.name === "NotAllowedError") {
+          this.setState({ error: "Permission to access sensor was denied." });
+        } else if (event.error.name === "NotReadableError") {
+          this.setState({ error: "Cannot connect to the sensor." });
+        }
+      });
 
-    sensor.addEventListener("reading", (e) => this.readSensor(e));
-    sensor.start();
-    /* eslint-enable no-undef */
+      sensor.addEventListener("reading", (e) => this.readSensor(e));
+      sensor.start();
+      /* eslint-enable no-undef */
+    } catch (error) {
+      // Handle construction errors.
+      if (error.name === "SecurityError") {
+        this.setState({
+          error: "Sensor construction was blocked by the Feature Policy.",
+        });
+      } else if (error.name === "ReferenceError") {
+        this.setState({ error: "Sensor is not supported by the User Agent." });
+      } else {
+        throw error;
+      }
+    }
   }
 
   readSensor(e) {
@@ -64,9 +87,11 @@ class Controller extends React.Component {
     });
 
     pickr.on("change", (instance) => {
-      let currColor = "#" + pickr.getColor().toHEXA().join("");
-      document.querySelector("#bar").style.backgroundColor = currColor;
-      this.socket.emit("colour", currColor);
+      if (!this.state.error) {
+        let currColor = "#" + pickr.getColor().toHEXA().join("");
+        document.querySelector("#bar").style.backgroundColor = currColor;
+        this.socket.emit("colour", currColor);
+      }
     });
 
     // pickr.on("hide", (instance) => {
@@ -75,47 +100,59 @@ class Controller extends React.Component {
     // });
 
     this.socket.on("colour", (colour) => {
-      console.log(colour);
-      pickr.setColor(colour);
-      document.querySelector("#bar").style.backgroundColor = colour;
+      if (!this.state.error) {
+        console.log(colour);
+        pickr.setColor(colour);
+        document.querySelector("#bar").style.backgroundColor = colour;
+      }
     });
   }
 
   render() {
-    return (
-      <div className=" w-screen h-screen bg-black overflow text-white flex flex-wrap">
-        <div className="w-full bg-gray-300 bar" id="bar"></div>
-        <div
-          className=" drawBtn bg-black w-full  noselect flex"
-          onTouchStart={() => this.socket.emit("draw", true)}
-          onTouchEnd={() => this.socket.emit("draw", false)}
-        >
-          <div className=" h-full w-full m-auto p-4">
-            <div className="border-b-8 border-blue-600 hoverbtn m-auto noselect text-gray-500 flex bg-gray-900 shadow-2xl  w-full rounded-lg h-full">
-              <p className="m-auto">DRAW</p>
+    if (this.state.error) {
+      return (
+        <div className="text-white flex w-screen h-screen">
+          <p className="m-auto text-2xl p-4 bg-red-800 rounded-lg">
+            {this.state.error}
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div className=" w-screen h-screen bg-black overflow text-white flex flex-wrap">
+          <div className="w-full bg-gray-300 bar" id="bar"></div>
+          <div
+            className=" drawBtn bg-black w-full  noselect flex"
+            onTouchStart={() => this.socket.emit("draw", true)}
+            onTouchEnd={() => this.socket.emit("draw", false)}
+          >
+            <div className=" h-full w-full m-auto p-4">
+              <div className="border-b-8 border-blue-600 hoverbtn m-auto noselect text-gray-500 flex bg-gray-900 shadow-2xl  w-full rounded-lg h-full">
+                <p className="m-auto">DRAW</p>
+              </div>
+            </div>
+          </div>
+          <div className=" w-full noselect flex">
+            <div
+              className=" calBtn w-1/2 noselect flex p-4"
+              onClick={() => this.socket.emit("calibrate")}
+            >
+              <div className="border-b-8 border-green-600 hoverbtn m-auto noselect flex bg-gray-900 text-gray-500  w-full h-full shadow-2xl rounded-lg">
+                <p className="m-auto">CALIBRATE</p>
+              </div>
+            </div>
+            <div
+              className=" delBtn text-gray-500 w-1/2 noselect flex p-4"
+              onClick={() => this.socket.emit("delete")}
+            >
+              <div className="border-b-8 border-red-600 hoverbtn m-auto noselect flex bg-gray-900 shadow-2xl  w-full h-full rounded-lg">
+                <p className="m-auto">CLEAR</p>
+              </div>
             </div>
           </div>
         </div>
-        <div className=" w-full noselect flex">
-          <div
-            className=" calBtn w-1/2 noselect flex p-4"
-            onClick={() => this.socket.emit("calibrate")}
-          >
-            <div className="border-b-8 border-green-600 hoverbtn m-auto noselect flex bg-gray-900 text-gray-500  w-full h-full shadow-2xl rounded-lg">
-              <p className="m-auto">CALIBRATE</p>
-            </div>
-          </div>
-          <div
-            className=" delBtn text-gray-500 w-1/2 noselect flex p-4"
-            onClick={() => this.socket.emit("delete")}
-          >
-            <div className="border-b-8 border-red-600 hoverbtn m-auto noselect flex bg-gray-900 shadow-2xl  w-full h-full rounded-lg">
-              <p className="m-auto">CLEAR</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
