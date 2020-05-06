780b2362a2a33a6a3adba471d6eab675b9515ad4
theraspberryguy <21122782+theraspberryguy@users.noreply.github.com>
2020-05-06 10:48:28 +0100

    Logging Update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var app = require("express")();
 var path = require("path");
+const { createLogger, format, transports, addColors } = require("winston");
 var server = require("http").Server(app);
 const https = require("https");
 
@@ -14,29 +15,47 @@ if (process.env.NODE_ENV === "production") {
 var express = require("express");
 var Session = require("./Session.js");
 
+const myCustomLevels = {
+  levels: {
+    "Num. Sessions": 0,
+    "Latest Sessions": 1,
+    Connected: 2,
+  },
+  colors: {
+    "Num. Sessions": "red",
+    "Latest Sessions": "blue",
+    Connected: "green",
+  },
+};
+
+const logger = createLogger({
+  level: "Connected",
+  levels: myCustomLevels.levels,
+  format: format.combine(format.colorize(), format.simple()),
+  transports: [new transports.Console()],
+});
+
+addColors(myCustomLevels.colors);
+
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-const wakeUpDyno = () => {
-  setInterval(() => {
-    https.get("https://paintr.sanjeet.co/").on("error", (err) => {
-      console.log("Ping Error: " + err.message);
-    });
-  }, 1500000);
-};
-
-server.listen(process.env.PORT || 80, () => {
-  wakeUpDyno();
-});
+server.listen(process.env.PORT || 80);
 
 let liveSessions = {};
 
 setInterval(() => {
-  console.log(Object.keys(liveSessions));
-}, 5000);
+  logger.log("Connected", Object.keys(liveSessions).length);
+  logger.log("Num. Sessions", Object.keys(liveSessions).length);
+  logger.log(
+    "Latest Sessions",
+    " " + Object.keys(liveSessions).slice(0).slice(-8)
+  );
+  // console.log(Object.keys(liveSessions).slice(0, 10));
+}, 3000);
 
 io.on("connection", (client) => {
   let roomID = client.handshake.query["room"];
