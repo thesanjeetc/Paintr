6a5c444651021586aa939e66fc2524a9f7098e17
theraspberryguy <21122782+theraspberryguy@users.noreply.github.com>
2020-05-07 12:33:05 +0100

    UI Update

--- client/src/Pages/Canvas.js
+++ client/src/Pages/Canvas.js
@@ -10,7 +10,7 @@ class Canvas extends React.Component {
 
     let address =
       process.env.NODE_ENV === "production"
-        ? "http://paintr-app.sanjeet.co/"
+        ? "https://paintr-app.sanjeet.co/"
         : window.location.hostname + "/";
 
     this.socket = io.connect(address + roomID, {
--- client/src/Pages/Controller.js
+++ client/src/Pages/Controller.js
@@ -14,7 +14,7 @@ class Controller extends React.Component {
 
     let address =
       process.env.NODE_ENV === "production"
-        ? "http://paintr-app.sanjeet.co/"
+        ? "https://paintr-app.sanjeet.co/"
         : window.location.hostname + "/";
 
     this.socket = io.connect(address + roomID, {
